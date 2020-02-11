095d182c5e72439747ad6bb81e747634e045081d
heyanlong <yanlong.hee@gmail.com>
2020-02-11 12:35:37 +0800

    Release 3.2.5

--- agent/cmd/main.go
+++ agent/cmd/main.go
@@ -20,8 +20,9 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "sky_php_agent"
 	app.Usage = "the skywalking trace sending agent"
+	app.Version = "3.2.5"
 	app.Flags = []cli.Flag{
-		&cli.StringSliceFlag{Name: "grpc", Usage: "SkyWalking collector grpc address", Value: cli.NewStringSlice("127.0.0.1:11800", "127.0.0.2:11800")},
+		&cli.StringSliceFlag{Name: "grpc", Usage: "SkyWalking collector grpc address", Value: cli.NewStringSlice("127.0.0.1:11800")},
 		&cli.StringFlag{Name: "socket", Usage: "Pipeline for communicating with PHP", Value: "/var/run/sky-agent.sock"},
 		&cli.IntFlag{Name: "send-rate", Usage: "Send trace 1 second by default", Value: 1},
 	}
