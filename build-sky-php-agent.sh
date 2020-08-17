9fcaae8a46f677d256d05b154024f736b2eb8de2
heyanlong <yanlong.hee@gmail.com>
2020-08-17 15:47:29 +0800

    remove modules

--- build-sky-php-agent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-GOOS=darwin GOARCH=amd64 go build -o sky-php-agent-darwin-x64 ./cmd/main.go
-GOOS=darwin GOARCH=386 go build -o sky-php-agent-darwin-x86 ./cmd/main.go
-
-GOOS=linux GOARCH=amd64 go build -o sky-php-agent-linux-x64 ./cmd/main.go
-GOOS=linux GOARCH=386 go build -o sky-php-agent-linux-x86 ./cmd/main.go
-
-GOOS=linux GOARCH=arm go build -o sky-php-agent-linux-arm86 ./cmd/main.go
-GOOS=linux GOARCH=arm64 go build -o sky-php-agent-linux-arm64 ./cmd/main.go
