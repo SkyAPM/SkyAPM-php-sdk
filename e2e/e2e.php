b0c43491972b778115f63732eab530407f0118e9
heyanlong <yanlong.hee@gmail.com>
2020-09-01 12:49:05 +0800

    Add fpm

--- e2e/e2e.php
+++ e2e/e2e.php
@@ -2,41 +2,87 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-function info($msg) {
-    echo chr(27) . "[1;34m" . $msg . chr(27) . "[0m\n";
+class E2E {
+//     public $url = "http://122.112.182.72:8080/graphql";
+    public $url = "http://127.0.0.1:12800/graphql";
+    public $services = <<<'GRAPHQL'
+query queryServices($duration: Duration!,$keyword: String!) {
+    services: searchServices(duration: $duration, keyword: $keyword) {
+        key: id
+        label: name
+    }
+}
+GRAPHQL;
+
+    public $startTime;
+
+    public function __construct() {
+        $this->startTime = date("Y-m-d His");
+    }
+
+    public function services() {
+        $client = new \GuzzleHttp\Client();
+        $res = $client->request("POST", $this->url, [
+            'json' => [
+                'query' => $this->services,
+                'variables' => [
+                    'duration' => [
+                        "start" => $this->startTime,
+                        "end" => date("Y-m-d His"),
+                        "step" => "SECOND"
+                    ],
+                    "keyword" => ""
+                ]
+            ]
+        ]);
+
+        if ($res->getStatusCode() != 200) {
+            return "";
+        }
+
+        return $res->getBody()->getContents();
+    }
+
+    public function info($msg) {
+        echo chr(27) . "[1;34m" . $msg . chr(27) . "[0m\n";
+    }
+
+    public function call() {
+        $ch = curl_init('http://127.0.0.1:8080/call');
+        curl_exec($ch);
+    }
+
+    public function check_services() {
+        $this->call();
+        sleep(1);
+        $res = $this->services();
+        var_dump($res);
+        return [$res, $res != ""];
+    }
+
 }
 
-function call() {
-    $ch = curl_init('http://127.0.0.1:8080/call');
-    curl_exec($ch);
-}
-
-function check_xx() {
-    call();
-    sleep(1);
-    // todo graphql
-    return [0, false];
-}
-
-$check = ['check_xx'];
+$check = ['check_services'];
+$e2e = new E2E();
 
 foreach($check as $func) {
-    info('exec ' . $func);
+    $e2e->info('exec ' . $func);
 
-    list($result, $status) = $func();
-    
     $status = false;
     for($i = 1; $i <= 10; $i++) {
-         info("test $func $i/10...");
-         list($result, $status) = $func();
+         $e2e->info("test $func $i/10...");
+         list($result, $status) = $e2e->$func();
          if ($status === true) {
              $status = true;
              break;
          }
+         sleep(1);
     }
 
     if (!$status) {
-        info("test $func fail...");
+        $e2e->info("test $func fail...");
         exit(2);
     }
+
+    $e2e->info("test $func success...");
 }
