3a6694ded9a603c7280695136362acdb09db5957
何延龙 <yanlong.hee@gmail.com>
2020-10-27 21:24:27 +0800

    Support Hyperf (#285)

--- src/sky_execute.cc
+++ src/sky_execute.cc
@@ -38,14 +38,14 @@ void sky_execute_ex(zend_execute_data *execute_data) {
     // swoole
     bool swoole = false;
     zval *sw_response;
-    if (function_name != nullptr && strcmp(function_name, "{closure}") == 0) {
+    if (SKY_IS_SWOOLE(function_name) || SKY_IS_HYPERF(class_name, function_name)) {
         uint32_t arg_count = ZEND_CALL_NUM_ARGS(execute_data);
         if (arg_count == 2) {
             zval *sw_request = ZEND_CALL_ARG(execute_data, 1);
             sw_response = ZEND_CALL_ARG(execute_data, 2);
             if (Z_TYPE_P(sw_request) == IS_OBJECT && Z_TYPE_P(sw_response) == IS_OBJECT) {
                 if (strcmp(ZSTR_VAL(Z_OBJ_P(sw_request)->ce->name), "Swoole\\Http\\Request") == 0) {
-                    if(strcmp(ZSTR_VAL(Z_OBJ_P(sw_response)->ce->name), "Swoole\\Http\\Response") == 0) {
+                    if (strcmp(ZSTR_VAL(Z_OBJ_P(sw_response)->ce->name), "Swoole\\Http\\Response") == 0) {
                         swoole = true;
                         sky_request_init(sw_request);
                     }
