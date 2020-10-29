e2d48402a5792473fe7b226a5bdf1fa7995005ad
heyanlong <yanlong.hee@gmail.com>
2020-10-29 17:45:29 +0800

    Redis

--- src/sky_redis.cc
+++ src/sky_redis.cc
@@ -11,35 +11,36 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 #include "sky_redis.h"
 
-Span *sky_redis(zend_execute_data *execute_data, char *class_name, char *function_name) {
-    std::string _class_name(class_name);
-    std::string _function_name(function_name);
+std::map<std::string, redis_cmd_cb> commands = {
+        {"DECR",   sky_redis_key_cmd},
+        {"GET",    sky_redis_key_cmd},
+        {"INCR",   sky_redis_key_cmd},
+        {"STRLEN", sky_redis_key_cmd},
+};
 
-    uint32_t args = ZEND_CALL_NUM_ARGS(execute_data);
-    char *fnamewall = sky_redis_fnamewall(_function_name.c_str());
-    if (args && sky_redis_opt_for_string_key(fnamewall) == 1) {
-        auto *segment = static_cast<Segment *>(SKYWALKING_G(segment));
-        auto *span = segment->createSpan(SkySpanType::Exit, SkySpanLayer::Cache, 7);
-        span->setOperationName(_class_name + "->" + _function_name);
-        span->addTag("db.type", "redis");
+Span *sky_redis(zend_execute_data *execute_data, const std::string &class_name, const std::string &function_name) {
 
-        // peer
-        auto peer = sky_redis_peer(execute_data);
-        if (!peer.empty()) {
-            span->setPeer(peer);
-        }
+    auto *segment = static_cast<Segment *>(SKYWALKING_G(segment));
+    auto *span = segment->createSpan(SkySpanType::Exit, SkySpanLayer::Cache, 7);
+    span->setOperationName(class_name + "->" + function_name);
+    span->addTag("db.type", "redis");
 
-        // key and command
-        sky_redis_command(span, execute_data, function_name);
+    // peer
+    auto peer = sky_redis_peer(execute_data);
+    if (!peer.empty()) {
+        span->setPeer(peer);
+    }
 
-        efree(fnamewall);
-        return span;
-    }   
+    if (commands.count(function_name) > 0) {
+        std::string cmd;
+        std::transform(function_name.begin(), function_name.end(), cmd.begin(), ::toupper);
+        span->addTag("redis.command", commands[cmd](execute_data, cmd));
+    }
 
-    efree(fnamewall);
-    return nullptr;
+    return span;
 }
 
 std::string sky_redis_peer(zend_execute_data *execute_data) {
@@ -56,77 +57,20 @@ std::string sky_redis_peer(zend_execute_data *execute_data) {
 
     if (!Z_ISUNDEF(host) && !Z_ISUNDEF(port) && Z_TYPE(host) == IS_STRING && Z_TYPE(port) == IS_LONG) {
         const char *char_host = ZSTR_VAL(Z_STR(host));
-        return std::string(char_host)+":"+std::to_string(Z_LVAL(port));
+        return std::string(char_host) + ":" + std::to_string(Z_LVAL(port));
     }
 
     return nullptr;
 }
 
-void sky_redis_command(Span *span, zend_execute_data *execute_data, char *function_name){
-    smart_str command = {0};
+std::string sky_redis_key_cmd(zend_execute_data *execute_data, std::string cmd) {
     uint32_t arg_count = ZEND_CALL_NUM_ARGS(execute_data);
-    char *fname = zend_str_tolower_dup(function_name, strlen(function_name));
-    smart_str_appends(&command, fname);
-    smart_str_appends(&command, " ");
-    efree(fname);
-
-    int is_string_command = 1;
-    uint32_t i;
-    for (i = 1; i < arg_count + 1; ++i)
-    {
-        zval str_p;
-        zval *p = ZEND_CALL_ARG(execute_data, i);
-        if (Z_TYPE_P(p) == IS_ARRAY){
-            is_string_command = 0;
-            break;
+    if (arg_count == 1) {
+        zval *key = ZEND_CALL_ARG(execute_data, 1);
+        if (Z_TYPE_P(key) == IS_STRING) {
+            return cmd + " " + std::string(Z_STRVAL_P(key));
         }
-
-        ZVAL_COPY(&str_p, p);
-        if (Z_TYPE_P(&str_p) != IS_STRING){
-            convert_to_string(&str_p);
-        }
-
-        if (i == 1){
-            span->addTag("redis.key", Z_STRVAL_P(&str_p));
-        }
-
-        char *tmp = zend_str_tolower_dup(Z_STRVAL_P(&str_p), Z_STRLEN_P(&str_p));
-        smart_str_appends(&command, tmp);
-        smart_str_appends(&command, " ");
-        efree(tmp);
     }
 
-    // store command to tags
-    if (command.s) {
-        smart_str_0(&command);
-        if (is_string_command){
-            std::string str_cmd = std::string(ZSTR_VAL(command.s));
-            int pos = str_cmd.find(" ");
-            if (pos >= 0) {
-                span->addTag("redis.command", str_cmd.substr(0, pos));
-            }
-        }
-        smart_str_free(&command);
-    }
-}
-
-int sky_redis_opt_for_string_key(char *fnamewall) {
-    if (strstr(REDIS_KEY_STRING, fnamewall) || \
-        strstr(REDIS_KEY_KEY, fnamewall) || \
-        strstr(REDIS_KEY_HASH, fnamewall) || strstr(REDIS_KEY_LIST, fnamewall) || \
-        strstr(REDIS_KEY_SET, fnamewall) || strstr(REDIS_KEY_SORT, fnamewall) || \
-        strstr(REDIS_KEY_HLL, fnamewall) || strstr(REDIS_KEY_GEO, fnamewall))
-    {
-        return 1;
-    }
-    return 0;
-}
-
-char *sky_redis_fnamewall(const char *function_name) {
-    char *fnamewall = (char *)emalloc(strlen(function_name) + 3);
-    bzero(fnamewall, strlen(function_name) + 3);
-    sprintf(fnamewall, "|%s|", function_name);
-    char *fnamewall_lower = zend_str_tolower_dup(fnamewall, strlen(fnamewall));
-    efree(fnamewall);
-    return fnamewall_lower;
+    return nullptr;
 }
--- src/sky_redis.h
+++ src/sky_redis.h
@@ -11,30 +11,24 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 #ifndef SKYWALKING_SKY_REDIS_H
 #define SKYWALKING_SKY_REDIS_H
 
 #include "php_skywalking.h"
 #include "sky_utils.h"
 #include <string>
+#include <map>
 #include "span.h"
 #include "segment.h"
 
-Span *sky_redis(zend_execute_data *execute_data, char *class_name, char *function_name);
+Span *sky_redis(zend_execute_data *execute_data, const std::string &class_name, const std::string &function_name);
 
-#define REDIS_KEY_KEY "|dump|exists|expire|expireat|move|persist|pexpire|pexpireat|pttl|rename|renamenx|sort|ttl|type|"
-#define REDIS_KEY_STRING "|append|bitcount|bitfield|decr|decrby|get|getbit|getrange|getset|incr|incrby|incrbyfloat|psetex|set|setbit|setex|setnx|setrange|strlen|"
-#define REDIS_OPERATION_STRING "|bitop|"
-#define REDIS_KEY_HASH "|hdel|hexists|hget|hgetall|hincrby|hincrbyfloat|hkeys|hlen|hmget|hmset|hscan|hset|hsetnx|hvals|hstrlen|"
-#define REDIS_KEY_LIST "|lindex|linsert|llen|lpop|lpush|lpushx|lrange|lrem|lset|ltrim|rpop|rpush|rpushx|"
-#define REDIS_KEY_SET "|sadd|scard|sismember|smembers|spop|srandmember|srem|sscan|"
-#define REDIS_KEY_SORT "|zadd|zcard|zcount|zincrby|zrange|zrangebyscore|zrank|zrem|zremrangebyrank|zremrangebyscore|zrevrange|zrevrangebyscore|zrevrank|zscore|zscan|zrangebylex|zrevrangebylex|zremrangebylex|zlexcount|"
-#define REDIS_KEY_HLL "|pfadd|watch|"
-#define REDIS_KEY_GEO "|geoadd|geohash|geopos|geodist|georadius|georadiusbymember|"
+std::string sky_redis_key_cmd(zend_execute_data *execute_data, std::string cmd);
 
-int sky_redis_opt_for_string_key(char *fnamewall);
-char *sky_redis_fnamewall(const char *function_name);
+typedef std::function<std::string(zend_execute_data *execute_data, std::string)> redis_cmd_cb;
+
 std::string sky_redis_peer(zend_execute_data *execute_data);
-void sky_redis_command(Span *span, zend_execute_data *execute_data, char *function_name);
+
 
 #endif // SKYWALKING_SKY_REDIS_H
