77c8406f05aaa185be8cc2605cadd7835c6f7de0
heyanlong <yanlong.hee@gmail.com>
2021-02-01 16:24:47 +0800

    github action debug

--- src/sky_module.cc
+++ src/sky_module.cc
@@ -170,7 +170,13 @@ void sky_request_flush(zval *response, uint64_t request_id) {
 
     try {
         php_printf("msg %s", msg.c_str());
-        boost::interprocess::message_queue mq(boost::interprocess::open_or_create, "skywalking_queue", 1024, 20480);
+        boost::interprocess::message_queue mq(
+                boost::interprocess::open_or_create,
+                "skywalking_queue",
+                1024,
+                20480,
+                boost::interprocess::permissions(666)
+                );
         mq.send(msg.data(), msg.size(), 0);
     } catch(boost::interprocess::interprocess_exception &ex) {
         php_printf("msg exception %s", ex.what());
