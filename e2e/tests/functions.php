1378976e994f1179f34e3a520761dfb2f6d32edb
何延龙 <yanlong.hee@gmail.com>
2021-02-24 19:30:58 +0800

    Delete error log

--- e2e/tests/functions.php
+++ e2e/tests/functions.php
@@ -11,10 +11,4 @@ function testAddLog(){
     $key = "info";
     $log = "test skywalking_log funciton, the third parameter [is_error] default is false.";
     skywalking_log($key, $log);
-
-    $key = "info";
-    $log = "test skywalking_log funciton, is_error set to true";
-    $is_error = true;
-    skywalking_log($key, $log, $is_error);
-
 }
