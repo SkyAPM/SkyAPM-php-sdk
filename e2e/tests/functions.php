11cd18dc8b7e97f7646a047d15bae51fcbe62f41
heyanlong <yanlong.hee@gmail.com>
2021-03-02 17:12:00 +0800

    Fix tag and log

--- e2e/tests/functions.php
+++ e2e/tests/functions.php
@@ -4,11 +4,12 @@ function testAddTag()
 {
     $key = "key";
     $value = "value";
-    skywalking_tag($key, $value);
+    skywalking_tag(__METHOD__, $key, $value);
 }
 
 function testAddLog(){
     $key = "info";
     $log = "test skywalking_log funciton, the third parameter [is_error] default is false.";
-    skywalking_log($key, $log);
+    skywalking_log(__METHOD__, $key, $log);
+    skywalking_log(__METHOD__, $key, $log, 1);
 }
