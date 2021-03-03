ffd96643563318c0d832ccbf7b82245247f8e1b7
heyanlong <yanlong.hee@gmail.com>
2021-03-03 13:37:17 +0800

    Fix tag and log

--- e2e/e2e.php
+++ e2e/e2e.php
@@ -234,7 +234,7 @@ foreach($check as $func) {
 
     if (!$status) {
         $e2e->info("test $func fail...");
-        exit(0);
+        exit(2);
     }
 
     $e2e->info("test $func success...");
