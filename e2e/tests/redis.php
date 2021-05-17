9c4601a69ebf54b1e9a08c03634423b7f9fe0fee
Bostin.wang <wangbo78978@126.com>
2021-05-17 16:56:14 +0800

    add bitpos/bitop/setbit/getbit/incrby/decrby/incrbyfloat/setrange/getrange/mset/msetnx/eval (#367)
    * 补充待实现的redis command为todo_cmd; 添加bitpos/bitop/setbit/getbit/incrby/decrby/incrbyfloat/setrange/getrange/mset/msetnx/eval命令的支持
    
    * 修复eval参数顺序错误的bug

--- e2e/tests/redis.php
+++ e2e/tests/redis.php
@@ -3,6 +3,7 @@
 function testRedis() {
     $redis = new Redis();
     $redis->connect('127.0.0.1', 6379);
+    $redis->select(1);
 
     $key = 'skywalking';
     // strings
@@ -10,23 +11,41 @@ function testRedis() {
     $redis->bitcount($key);
     $redis->bitcount($key, 0);
     $redis->bitcount($key, 0, 1);
+    $redis->bitpos($key, 0, 1, 2);
+    $redis->bitop('AND', $key, $key . '_01', $key . '_02', $key . '_03');
+    $redis->setbit($key, 1, 1);
+    $redis->setbit($key, 1, 0);
+    $redis->setbit($key, 1, true);
+    $redis->setbit($key, 1, false);
+    $redis->getbit($key, 1);
     $redis->decr($key);
     $redis->get($key);
     $redis->getSet($key, "test");
     $redis->incr($key);
+    $redis->incrBy($key, 10);
+    $redis->decrBy($key, 10);
+    $redis->incrByFloat($key, 10.5);
     $redis->setnx($key, "test");
     $redis->strlen($key);
     $redis->set($key, "test");
     $redis->set($key, "test", 100);
     $redis->set($key, "test", ["nx", "ex" => 200]);
     $redis->setex($key, 300, "test");
+    $redis->setrange($key, 1, "222");
+    $redis->getrange($key, 1, 4);
 
     // multiple keys
     $redis->mget([$key .'_1', $key . '_2', $key . '_3']);
     $redis->getMultiple([$key .'_1', $key . '_2', $key . '_3']);
 
+    // multiple key-value
+    $redis->mset([$key . '_1' => '111', $key . '_2' => '222', $key . '_3' => '333']);
+    $redis->msetnx([$key . '_1' => '111', $key . '_2' => '222', $key . '_3' => '333']);
+
     // uncertain keys
     $redis->eval('return {1,2,3};');
+    $redis->eval('return {1,2,3};', ['val_01', 'val_02']);
+    $redis->eval('return {1,2,3};', [$key . '_01', $key . '_02', 'val_01', 'val_02'], 2);
     $redis->del($key);
     $redis->del($key, $key . '_1');
     $redis->del([$key, $key . '_1', $key . '_2']);
